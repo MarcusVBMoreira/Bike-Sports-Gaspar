41c91a6b094b47d590a00148b8840a23da425af2
maria <maria@DESKTOP-KA828E2>
2024-02-27 21:52:30 -0300

    Incluindo o arquivo conecta.php e configurando a coneção do banco de dados com o futuro servidor e máquina local

---
+++ painel/conecta.php
@@ -0,0 +1,34 @@
+<?php
+    
+    //*CONEXÃO DO SITE COM O BANDO DE DADOS*
+
+    //!----------------------------------------------------------!/
+
+    //?Conexão com o servidor
+    //$servidor = "localhost";
+    //$usuario = "";
+    //$senha = "";
+    //$banco = "bike_sport_gaspar";
+
+    //!----------------------------------------------------------!/
+
+    //?Coneão com maquina local
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "bike_sport_gaspar";
+
+    //!----------------------------------------------------------!/
+
+    $conexao = new mysqli($servidor,$usuario,$senha,$banco);
+
+    //!----------------------------------------------------------!/
+
+    //?Verificando de o banco foi conectado com sucesso ou não
+    if(mysqli_connect_errno()){
+        echo "ERRO DE CONEXÃO";
+    }else{
+        echo "BANCO CONECTADO";
+    }
+
+?>
