070f2167913246345d00929e33535684aa4235cb
maria <maria@DESKTOP-KA828E2>
2024-03-05 21:08:49 -0300

    adicionando as logos na pasta img

--- index.php
+++ index.php
@@ -12,66 +12,68 @@
     <link rel="stylesheet" href="assets/css/header_footer.css">
     <link rel="stylesheet" href="assets/css/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- !Favicon! -->
+    <link rel="shortcut icon" alt="logo DKS" href="assets/img/logoBSG-branco-menor.ico" type="image/x-icon">
     <title>Sport Gaspar</title>
   </head>
   <body>
     <header>
       <nav class="nav-1">
-          <a href="index.php" class="logo">Logo</a>
-          <div class="pesquisar">
-              <input type="text" class="pesquisa" placeholder="Pesquisar...">
-              <input type="submit" value="Pesquisar">
-          </div>
-          <ul class="nav-list-1">
-              <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
-              <li><a href="#"><i class="fa-solid fa-heart"></i></a></li>
-              <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
-          </ul>
+        <a href="index.php" class="logo"><img src="assets/img/logoBSG-preto-menor.png"></img></a>
+        <div class="pesquisar">
+            <input type="text" class="pesquisa" placeholder="Pesquisar...">
+            <input type="submit" value="Pesquisar">
+        </div>
+        <ul class="nav-list-1">
+            <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+            <li><a href="#"><i class="fa-solid fa-heart"></i></a></li>
+            <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
+        </ul>
       </nav>
       <nav class="nav-2">
-          <div class="categorias">
-              <i class="fa-solid fa-list"></i>
-              <h2>categorias</h2>
-          </div>
-          <ul class="nav-list-2">
-              <li>
-                  <a href="">Bike</a>
-                  <ul class="dropdown">
-                      <li><a href="montain_bike.php">Montain Bike</a></li>
-                      <li><a href="">Eletric Bike</a></li>
-                      <li><a href="">Road Bike</a></li>
-                      <li><a href="">Simple Bike</a></li>
-                  </ul> 
-              </li>
-              <li><a href="">Acessorios</a></li>
-              <li><a href="">Roupas</a></li>
-              <li><a href="">Ferramentas</a></li>
-              <li><a href="">Nutrição</a></li>
-          </ul>
+        <div class="categorias">
+            <i class="fa-solid fa-list"></i>
+            <h2>categorias</h2>
+        </div>
+        <ul class="nav-list-2">
+            <li>
+                <a href="">Bike</a>
+                <ul class="dropdown">
+                    <li><a href="montain_bike.php">Montain Bike</a></li>
+                    <li><a href="">Eletric Bike</a></li>
+                    <li><a href="">Road Bike</a></li>
+                    <li><a href="">Simple Bike</a></li>
+                </ul> 
+            </li>
+            <li><a href="">Acessorios</a></li>
+            <li><a href="">Roupas</a></li>
+            <li><a href="">Ferramentas</a></li>
+            <li><a href="">Nutrição</a></li>
+        </ul>
       </nav>
     </header>
     <main class="main-banner">
-        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="10000">
-              <img src="assets/img/banner-1.png" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="2000">
-              <img src="assets/img/banner-2.png" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="assets/img/banner-3.png" class="d-block w-100" alt="...">
-            </div>
+      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img src="assets/img/banner-1.png" class="d-block w-100" alt="...">
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img src="assets/img/banner-2.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/banner-3.png" class="d-block w-100" alt="...">
+          </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </main>
     <div class="container">
       <!-- Espaço para a area de destaque -->
