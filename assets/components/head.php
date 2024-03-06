9899210e62de4f4ae6d62f84a6df2d45d65c76a1
maria <maria@DESKTOP-KA828E2>
2024-03-06 18:59:33 -0300

    organização das pastas novas de componentes e css, padronização das fontes e cores

--- assets/components/head.php
+++ assets/components/head.php
@@ -5,9 +5,9 @@
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <!-- !RESET CSS! -->
-    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/reset/reset.css">
     <!-- Links CSS -->
-    <link rel="stylesheet" href="assets/css/padrao.css">
+    <link rel="stylesheet" href="assets/css/padrao/padrao.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <!-- !Favicon! -->
     <link rel="shortcut icon" alt="logo DKS" href="assets/img/logoBSG-branco-menor.ico" type="image/x-icon">
--- index.php
+++ index.php
@@ -46,7 +46,6 @@
       <div class="line"></div>
       <p>&copy; Code Track</p>
     </footer> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/banner_rotate.js"></script>
+    
   </body>
 </html>
