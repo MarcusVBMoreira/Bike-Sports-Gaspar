d701cdbc76536b025de61a13e3702a964f8bb03c
maria <maria@DESKTOP-KA828E2>
2024-03-17 20:34:10 -0300

    inicio da validação de formulário

--- app/assets/components/cadastro.php
+++ app/assets/components/cadastro.php
@@ -11,49 +11,68 @@
                         <img src="assets/img/logoBSG-preto-menor.png" alt="logo BSG">
                     </div>
                 </div>
-                <form action="" class="forms_login">
+                <form action="#" class="forms_login" id="forms_cadastro">
                     <div class="components_forms">
                         <div class="titulo_login_cadastro">
                             <h1 class="titulo">Cadastro</h1>
                         </div>
                         <div class="item_forms">
                             <input type="text" placeholder="Nome" name="" id="nome" class="regular">
+                            <span class="regular" id="span_nome">Informar seu nome</span>
                         </div>
                         <div class="item_forms">
-                            <input type="text" placeholder="Sobrenome" name="CPF" id="CPF" class="regular">
+                            <input type="text" placeholder="Sobrenome" name="sobrenome" id="sobrenome" class="regular">
+                            <span class="regular" id="span_sobrenome">Informar seu Sobrenome</span>
                         </div>
                         <div class="item_forms">
-                            <input type="tel" placeholder="Telefone" name="telefone" id="telefone" class="regular">
+                            <input type="text" placeholder="Telefone" name="telefone" id="telefone" class="regular">
+                            <span class="regular" id="span_telefone">Informar seu telefone</span>
                         </div>
                         <div class="item_forms">
-                            <input type="email" placeholder="E-mail" name="email" id="email" class="regular">
+                            <input type="text" placeholder="Data de nascimento" name="data_nascimento" id="data_nascimento" class="regular">
+                            <span class="regular" id="span_data">Informar sua data de nascimento</span>
+                        </div>
+                        <div class="item_forms">
+                            <input type="text" placeholder="E-mail" name="email" id="email" class="regular">
+                            <span class="regular" id="span_email">Informar seu e-mail</span>
+                            <span class="regular" id="span_email_valido">Informar um e-mail valido</span>
                         </div>
                         <div class="item_forms">
                             <input type="password" placeholder="Senha" name="senha" id="senha" class="regular">
+                            <span class="regular" id="span_senha">Informar sua senha</span>
+                            <span class="regular" id="span_senha_valida">A senha deve ter ao menos 8 dígitos</span>
                         </div>
                         <div class="item_forms">
                             <input type="password" placeholder="Confirmar senha" name="confirmar_senha" id="confirmar_senha" class="regular">
+                            <span class="regular" id="span_confirmar_senha">Confirmar senha</span>
+                            <span class="regular" id="span_senha_diferente">A senha não é igual</span>
                         </div>
                         <div class="titulo_login_cadastro">
                             <h1 class="titulo">Endereço</h1>
                         </div>
                         <div class="item_forms">
                             <input type="text" placeholder="CEP" name="CEP" id="CEP" class="regular">
+                            <span class="regular" id="span_CEP">Informar o CEP</span>
                         </div>
                         <div class="item_forms">
                             <input type="text" placeholder="Estado" name="estado" id="estado" class="regular">
+                            <span class="regular" id="span_estado">Informar o estado</span>
                         </div>
                         <div class="item_forms">
                             <input type="text" placeholder="Cidade" name="cidade" id="cidade" class="regular">
+                            <span class="regular" id="span_cidade">Informar a cidade</span>
                         </div>
                         <div class="item_forms">
                             <input type="text" placeholder="Bairro" name="bairro" id="bairro" class="regular">
+                            <span class="regular" id="span_bairro">Informar o bairro</span>
                         </div>
                         <div class="item_forms">
                             <input type="text" placeholder="Rua" name="rua" id="rua" class="regular">
+                            <span class="regular" id="span_rua">Informar a rua</span>
                         </div>
                         <div class="item_forms">
                             <input type="text" placeholder="Número" name="numero" id="numero" class="regular">
+                            <span class="regular" id="span_numero">Informar o número</span>
                         </div>
                         <div class="item_forms">
                             <input type="text" placeholder="complemento" name="complemento" id="complemento" class="regular">
@@ -66,5 +85,8 @@
                 </div>
             </div>
         </div>
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+        <script src="assets/js/cadastro.js"></script>
     </body>
 </html>
---
+++ app/assets/js/cadastro.js
@@ -0,0 +1,292 @@
+// !MASK!
+$(document).ready(function () {
+    // Adicionando máscara ao campo de telefone
+    $("#telefone").mask("(00)00000-0000");
+
+    // Adicionando máscara ao campo de CPF
+    $("#CEP").mask("00000-000");
+
+    // Adicionando máscara ao campo de data de nascimento
+    $("#data_nascimento").mask("00/00/0000");
+});
+
+
+
+
+
+
+//!Validação do formulário!
+
+const form = document.querySelector("#forms_cadastro");
+
+const nome = document.querySelector("#nome");
+const spanNome = document.getElementById('span_nome');
+
+const sobrenome = document.querySelector("#sobrenome");
+const spanSobrenome = document.getElementById('span_sobrenome');
+
+const telefone = document.querySelector("#telefone");
+const spanTelefone = document.getElementById('span_telefone');
+
+const data = document.querySelector("#data_nascimento");
+const spanData = document.getElementById('span_data');
+
+const email = document.querySelector("#email");
+const spanEmail = document.getElementById('span_email');
+const spanEmailValido = document.getElementById('span_email_valido');
+
+const senha = document.querySelector("#senha");
+const spanSenha = document.getElementById('span_senha');
+const spanSenhaValida = document.getElementById('span_senha_valida');
+
+const confirmarSenha = document.querySelector("#confirmar_senha");
+const spanConfirmarSenha = document.getElementById('span_confirmar_senha');
+const spanSenhaDiferente = document.getElementById('span_senha_diferente');
+
+const CEP = document.querySelector("#CEP");
+const spanCEP = document.getElementById('span_CEP');
+
+const estado = document.querySelector("#estado");
+const spanEstado = document.getElementById('span_estado');
+
+const cidade = document.querySelector("#cidade");
+const spanCidade = document.getElementById('span_cidade');
+
+const bairro = document.querySelector("#bairro");
+const spanBairro = document.getElementById('span_bairro');
+
+const rua = document.querySelector("#rua");
+const spanRua = document.getElementById('span_rua');
+
+const numero = document.querySelector("#numero");
+const spanNumero = document.getElementById('span_numero');
+
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+
+    //!verificar se o nome está vazio
+    if(nome.value ===""){
+        nome.classList.add('active');
+        spanNome.classList.add('active');
+    }else{
+        nome.classList.remove('active');
+        spanNome.classList.remove('active');
+    }
+
+    //!verificar se sobrenome está vazio
+    if(sobrenome.value ===""){
+        sobrenome.classList.add('active');
+        spanSobrenome.classList.add('active');
+    }else{
+        sobrenome.classList.remove('active');
+        spanSobrenome.classList.remove('active');
+    }
+
+    //!verificar se o telefone está vazio
+    if(telefone.value ===""){
+        telefone.classList.add('active');
+        spanTelefone.classList.add('active');
+    }else{
+        telefone.classList.remove('active');
+        spanTelefone.classList.remove('active');
+    }
+
+    //!verificar se a data de nascimento está vazio
+    if(data.value ===""){
+        data.classList.add('active');
+        spanData.classList.add('active');
+    }else{
+        data.classList.remove('active');
+        spanData.classList.remove('active');
+    }
+
+    //!verificar se o email está vazio
+    if(email.value ===""){
+        email.classList.add('active');
+        spanEmail.classList.add('active');
+    }else{
+        email.classList.remove('active');
+        spanEmail.classList.remove('active');
+        //!Verificar se o email é valido
+        if(!isEmailValid(email.value)){
+            email.classList.add('active');
+            spanEmailValido.classList.add('active');
+        }else{
+            email.classList.remove('active');
+            spanEmailValido.classList.remove('active');
+        }
+    }
+    
+
+    //!verificar se a senha está vazio
+    if(senha.value ===""){
+        senha.classList.add('active');
+        spanSenha.classList.add('active');
+    }else{
+        senha.classList.remove('active');
+        spanSenha.classList.remove('active');
+        //!verificar se a senha tem no mínimo 8 dígitos
+        if(!validarSenha(senha.value, 8)){
+            senha.classList.add('active');
+            spanSenhaValida.classList.add('active');
+        }else{
+            senha.classList.remove('active');
+            spanSenhaValida.classList.remove('active');
+        }
+    }
+    
+
+    //!verificar se a confirmção de senha está vazio
+    if(confirmarSenha.value ===""){
+        confirmarSenha.classList.add('active');
+        spanConfirmarSenha.classList.add('active');
+    }else{
+        confirmarSenha.classList.remove('active');
+        spanConfirmarSenha.classList.remove('active');
+        //!Verificar se a confirmação de senha é igual a senha
+        if(!validarSenhaDiferente(senha.value, confirmarSenha.value)){
+            confirmarSenha.classList.add('active');
+            spanSenhaDiferente.classList.add('active');
+        }else{
+            confirmarSenha.classList.remove('active');
+            spanSenhaDiferente.classList.remove('active');
+        }
+    }
+    
+
+
+    //!verificar se o CEP está vazio
+    if(CEP.value ===""){
+        CEP.classList.add('active');
+        spanCEP.classList.add('active');
+    }else{
+        CEP.classList.remove('active');
+        spanCEP.classList.remove('active');
+    }
+
+    //!verificar se o estado está vazio
+    if(estado.value ===""){
+        estado.classList.add('active');
+        spanEstado.classList.add('active');
+    }else{
+        estado.classList.remove('active');
+        spanEstado.classList.remove('active');
+    }
+
+    //!verificar se a cidade está vazio
+    if(cidade.value ===""){
+        cidade.classList.add('active');
+        spanCidade.classList.add('active');
+    }else{
+        cidade.classList.remove('active');
+        spanCidade.classList.remove('active');
+    }
+
+    //!verificar se a Bairro está vazio
+    if(bairro.value ===""){
+        bairro.classList.add('active');
+        spanBairro.classList.add('active');
+    }else{
+        bairro.classList.remove('active');
+        spanBairro.classList.remove('active');
+    }
+
+    //!verificar se a rua está vazio
+    if(rua.value ===""){
+        rua.classList.add('active');
+        spanRua.classList.add('active');
+    }else{
+        rua.classList.remove('active');
+        spanRua.classList.remove('active');
+    }
+
+    //!verificar se o Numero está vazio
+    if(numero.value ===""){
+        numero.classList.add('active');
+        spanNumero.classList.add('active');
+    }else{
+        numero.classList.remove('active');
+        spanNumero.classList.remove('active');
+    }
+
+
+})
+
+
+//Função que valida email
+function isEmailValid(email){
+    //Criar uma regex para validar email
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if(emailRegex.test(email)){
+        return true;
+    }
+
+    return false;
+}
+
+//Função que valida a senha
+function validarSenha(senha, minDigts){
+    if(senha.length >= minDigts){
+        return true;
+    }
+
+    return false;
+}
+function validarSenhaDiferente(senha, confirmarSenha){
+    if(senha === confirmarSenha){
+        return true;
+    }
+
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// !CEP AUTOMÁTICO!
+$("#CEP").blur(function(){
+	// Remove tudo o que não é número para fazer a pesquisa
+	var cep = this.value.replace(/[^0-9]/, "");
+	
+	// Validação do CEP; caso o CEP não possua 8 números, então cancela
+	// a consulta
+	if(cep.length != 8){
+		return false;
+	}
+	
+	// A url de pesquisa consiste no endereço do webservice + o cep que
+	// o usuário informou + o tipo de retorno desejado (entre "json",
+	// "jsonp", "xml", "piped" ou "querty")
+	var url = "https://viacep.com.br/ws/"+cep+"/json/";
+	
+	// Faz a pesquisa do CEP, tratando o retorno com try/catch para que
+	// caso ocorra algum erro (o cep pode não existir, por exemplo) a
+	// usabilidade não seja afetada, assim o usuário pode continuar//
+	// preenchendo os campos normalmente
+	$.getJSON(url, function(dadosRetorno){
+		try{
+			// Preenche os campos de acordo com o retorno da pesquisa
+			$("#rua").val(dadosRetorno.logradouro);
+			$("#bairro").val(dadosRetorno.bairro);
+			$("#cidade").val(dadosRetorno.localidade);
+			$("#estado").val(dadosRetorno.uf);
+		}catch(ex){}
+	});
+});
