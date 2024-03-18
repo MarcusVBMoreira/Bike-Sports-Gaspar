0b3261328fdaaec769f38591bf6570cdf200d46a
Santozzz <davidss.fun@gmail.com>
2024-03-18 20:35:58 -0300

    iniciando a tela de detalhes do meus pedidos

---
+++ app/andamento-pedido.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        require('assets/components/head.php');
+    ?>
+</head>
+<body>
+    <?php
+        require('assets/components/header.php')
+    ?>
+    <?php
+        require('assets/components/andamento-pedido.php')
+    ?>
+    <?php
+        require('assets/components/footer.php')
+    ?>
+</body>
+</html>
