f5c8b1c4bc201dab9934d6d308cb3aa7796622c7
Santozzz <davidss.fun@gmail.com>
2024-03-20 15:56:45 -0300

    Tela de detalhes de produtos pronta

--- app/assets/components/andamento-pedido.php
+++ app/assets/components/andamento-pedido.php
@@ -9,7 +9,7 @@
         <div class="container-andamento">
             <div class="container-circle">
                 <div class="line"></div>
-                <div class="Mamae">
+                <div class="container-mae">
                     <div class="container-circle-description">
                         <div class="circle">
                             <img src="assets/img/clipboard.png" alt="">
@@ -41,6 +41,69 @@
                         <p>Finalizado</p>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="container-pedido-prince">
+            <div class="container-endereco">
+                <h1>Endereço de entrega :</h1>
+                <p>[Endereço]</p>
+                <p>[Cidade, estado, CEP]</p>
+                <div class="line-ende"></div>
+            </div>
+            <div class="container-codigo">
+                <div class="ttl-cod">
+                    <h1>Código do produto :</h1>
+                    <p>[ Código ]</p>
+                </div>
+                <div class="container-pedidos">
+                <div class="card-pedidos">
+                    <div class="img-prod">
+                        <img src="upload/produtos/2.png" alt="">
+                    </div>
+                    <div class="conteudo">
+                        <div class="nome-prod">
+                            <p class="txt-ped bold">Cambio Dianteiro Mtb Shimano Tz500 31.8 Puxa Por Baixo 42d</p>
+                        </div>
+                        <div class="valor">
+                            <p>[Valor]</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="line"></div>
+                <div class="card-pedidos">
+                    <div class="img-prod">
+                        <img src="upload/produtos/2.png" alt="">
+                    </div>
+                    <div class="conteudo">
+                        <div class="nome-prod">
+                            <p class="txt-ped bold">Cambio Dianteiro Mtb Shimano Tz500 31.8 Puxa Por Baixo 42d</p>
+                        </div>
+                        <div class="valor">
+                            <p>[Valor]</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="line"></div>
+                <div class="desc-produto">
+                    <div class="valores">
+                        <p class="bold">Subtotal de produtos:</p>
+                        <p class="bold">[ valor ]</p>
+                    </div>
+                    <div class="valores">
+                        <p class="bold">Frete:</p>
+                        <p class="bold">[ valor ]</p>
+                    </div>
+                    <div class="valores">
+                        <p class="bold">Descontos:</p>
+                        <p class="bold">[ valor ]</p>
+                    </div>
+                    <div class="valores">
+                        <p class="bold">Total do pedido:</p>
+                        <p class="bold">[ valor ]</p>
+                    </div>
+                    <p class="metodo">Metodo de pagamento: [ metodo ]</p>
+                </div>
+            </div>
             </div>
         </div>
     </div>
