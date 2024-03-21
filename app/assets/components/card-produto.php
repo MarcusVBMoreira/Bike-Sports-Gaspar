54b204c091bb4ce3da3325edc83ea08e7fd8b3f2
Santozzz <davidss.fun@gmail.com>
2024-03-21 16:50:47 -0300

    ajustes no card-produto

--- app/assets/components/card-produto.php
+++ app/assets/components/card-produto.php
@@ -9,22 +9,22 @@
         <?php else: ?>
             <?php foreach($produtos as $p): ?>
                 <div class="produto">
-                    <div class="coracao">
-                        <i class="fa-regular fa-heart"></i>
-                    </div>
                     <div class="img_produto">
                         <a href="infos-produto.php?codigo=<?= $p['codigo'] ?>" style="width: 100%;"><img src="upload/produtos/<?= $p['img1'] ?>" style="width: 100%; border-radius: 5%;" class="produto_img"></a>
+                    </div>
+                    <div class="fav-avali">
+                        <i class="fa-regular fa-heart"></i>
+                        <div class="avaliacao">
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                        </div>
                     </div>
                     <div class="nome_produto">
                         <p class="bold"><?= $p['nome'] ?></p>
                         <p class="regular">(cod. <?= $p['codigo'] ?>)</p>
-                    </div>
-                    <div class="avaliacao">
-                        <i class="fa-regular fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
                     </div>
                     <div class="valor_produto">
                         <p class="bold">R$ <?= $p['valor'] ?></p>
