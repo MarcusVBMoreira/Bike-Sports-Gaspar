d6db3b8ed5444ff8de2ae0cfecba009a040e02fa
Santozzz <davidss.fun@gmail.com>
2024-03-23 10:23:08 -0300

    ajustes no footer-responsivo

--- app/assets/components/footer-responsivo.php
+++ app/assets/components/footer-responsivo.php
@@ -84,5 +84,6 @@
             <div class="line"></div>
             <p class="copy">&copy; Code Track</p>
         </footer>
+    <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
