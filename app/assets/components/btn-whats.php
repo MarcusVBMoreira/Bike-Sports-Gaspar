42e44640e860c92e178154f83722368e5b04f6d7
Santozzz <davidss.fun@gmail.com>
2024-03-28 14:31:57 -0300

    adição do btn-serviços em cima do btn-whatz

--- app/assets/components/btn-whats.php
+++ app/assets/components/btn-whats.php
@@ -4,8 +4,17 @@
         <link rel="stylesheet" href="assets/css/components/btn-whats.css">
     </head>
     <body>
-        <a target="_blank" href="https://wa.me/5511940080872" class="pop-up">
-            <i class="fa-brands fa-whatsapp"></i>
-        </a>
+        <div class="contatos">
+            <div class="leo">
+                <a target="_blank" href="https://wa.me/5511940080872" class="pop-up">
+                    <i class="fa-solid fa-screwdriver-wrench"></i>                
+                </a>
+            </div>
+            <div class="ettore">
+                <a target="_blank" href="https://wa.me/5511940080872" class="pop-up">
+                    <i class="fa-brands fa-whatsapp"></i>
+                </a>
+            </div>
+        </div>
     </body>
 </html>
