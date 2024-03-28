d05ce491d300120bfeaa1f4895843127231510a6
maria <maria@DESKTOP-KA828E2>
2024-03-28 16:55:00 -0300

    ajuste menu responsivo

--- app/assets/js/mobile-menu.js
+++ app/assets/js/mobile-menu.js
@@ -9,29 +9,95 @@ function menu_clicou(){
 function bike_dropdown(){
     document.getElementById('bike').classList.toggle('active');
     document.getElementById('seta-bike').classList.toggle('fa-angle-down');
+
+    document.getElementById('ace').classList.remove('active');
+    document.getElementById('seta-ace').classList.remove('fa-angle-down');
+    document.getElementById('vest').classList.remove('active');
+    document.getElementById('seta-vest').classList.remove('fa-angle-down');
+    document.getElementById('fer').classList.remove('active');
+    document.getElementById('seta-fer').classList.remove('fa-angle-down');
+    document.getElementById('comp').classList.remove('active');
+    document.getElementById('seta-comp').classList.remove('fa-angle-down');
+    document.getElementById('sup').classList.remove('active');
+    document.getElementById('seta-sup').classList.remove('fa-angle-down');
 }
 
 function ace_dropdown(){
     document.getElementById('ace').classList.toggle('active');
     document.getElementById('seta-ace').classList.toggle('fa-angle-down');
+
+    document.getElementById('bike').classList.remove('active');
+    document.getElementById('seta-bike').classList.remove('fa-angle-down');
+    document.getElementById('vest').classList.remove('active');
+    document.getElementById('seta-vest').classList.remove('fa-angle-down');
+    document.getElementById('fer').classList.remove('active');
+    document.getElementById('seta-fer').classList.remove('fa-angle-down');
+    document.getElementById('comp').classList.remove('active');
+    document.getElementById('seta-comp').classList.remove('fa-angle-down');
+    document.getElementById('sup').classList.remove('active');
+    document.getElementById('seta-sup').classList.remove('fa-angle-down');
 }
 
 function vest_dropdown(){
     document.getElementById('vest').classList.toggle('active');
     document.getElementById('seta-vest').classList.toggle('fa-angle-down');
+
+    document.getElementById('bike').classList.remove('active');
+    document.getElementById('seta-bike').classList.remove('fa-angle-down');
+    document.getElementById('ace').classList.remove('active');
+    document.getElementById('seta-ace').classList.remove('fa-angle-down');
+    document.getElementById('fer').classList.remove('active');
+    document.getElementById('seta-fer').classList.remove('fa-angle-down');
+    document.getElementById('comp').classList.remove('active');
+    document.getElementById('seta-comp').classList.remove('fa-angle-down');
+    document.getElementById('sup').classList.remove('active');
+    document.getElementById('seta-sup').classList.remove('fa-angle-down');
 }
 
 function fer_dropdown(){
     document.getElementById('fer').classList.toggle('active');
     document.getElementById('seta-fer').classList.toggle('fa-angle-down');
+
+    document.getElementById('bike').classList.remove('active');
+    document.getElementById('seta-bike').classList.remove('fa-angle-down');
+    document.getElementById('ace').classList.remove('active');
+    document.getElementById('seta-ace').classList.remove('fa-angle-down');
+    document.getElementById('vest').classList.remove('active');
+    document.getElementById('seta-vest').classList.remove('fa-angle-down');
+    document.getElementById('comp').classList.remove('active');
+    document.getElementById('seta-comp').classList.remove('fa-angle-down');
+    document.getElementById('sup').classList.remove('active');
+    document.getElementById('seta-sup').classList.remove('fa-angle-down');
 }
 
 function comp_dropdown(){
     document.getElementById('comp').classList.toggle('active');
     document.getElementById('seta-comp').classList.toggle('fa-angle-down');
+
+    document.getElementById('bike').classList.remove('active');
+    document.getElementById('seta-bike').classList.remove('fa-angle-down');
+    document.getElementById('ace').classList.remove('active');
+    document.getElementById('seta-ace').classList.remove('fa-angle-down');
+    document.getElementById('vest').classList.remove('active');
+    document.getElementById('seta-vest').classList.remove('fa-angle-down');
+    document.getElementById('fer').classList.remove('active');
+    document.getElementById('seta-fer').classList.remove('fa-angle-down');
+    document.getElementById('sup').classList.remove('active');
+    document.getElementById('seta-sup').classList.remove('fa-angle-down');
 }
 
 function sup_dropdown(){
     document.getElementById('sup').classList.toggle('active');
     document.getElementById('seta-sup').classList.toggle('fa-angle-down');
+
+    document.getElementById('bike').classList.remove('active');
+    document.getElementById('seta-bike').classList.remove('fa-angle-down');
+    document.getElementById('ace').classList.remove('active');
+    document.getElementById('seta-ace').classList.remove('fa-angle-down');
+    document.getElementById('vest').classList.remove('active');
+    document.getElementById('seta-vest').classList.remove('fa-angle-down');
+    document.getElementById('fer').classList.remove('active');
+    document.getElementById('seta-fer').classList.remove('fa-angle-down');
+    document.getElementById('comp').classList.remove('active');
+    document.getElementById('seta-comp').classList.remove('fa-angle-down');
 }
