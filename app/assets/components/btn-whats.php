e47b5c063c9573487288b05853ae06b028d82cd4
Santozzz <davidss.fun@gmail.com>
2024-04-01 16:08:05 -0300

    criação da pag servicos

--- app/assets/components/btn-whats.php
+++ app/assets/components/btn-whats.php
@@ -6,7 +6,7 @@
     <body>
         <div class="contatos">
             <div class="leo">
-                <a target="_blank" href="https://wa.me/5511940080872" class="pop-up">
+                <a target="_blank" href="servicos.php" class="pop-up">
                     <i class="fa-solid fa-screwdriver-wrench"></i>                
                 </a>
             </div>
---
+++ app/assets/components/servicos.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <link rel="stylesheet" href="assets/css/components/servicos.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
