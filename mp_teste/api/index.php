6ce2ae7868bb454e676fdf6f53c8b6ff4e0e9e53
projeto-miguel <miguel2020carneiro@gmail.com>
2024-06-10 23:12:42 -0300

    manipulando uri com htaccess

--- mp_teste/api/index.php
+++ mp_teste/api/index.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/private/utilities/Response.php';
 $response = new Response($_SERVER['REQUEST_METHOD']);
 
 $response->AddToResponse('message','API running ok.');
-
+$response->AddToResponse('teste',$_GET);
 
 $response->Send(200);
