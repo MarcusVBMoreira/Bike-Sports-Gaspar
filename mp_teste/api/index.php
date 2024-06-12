208f5c3436b0ebde624e04602aaa442fa88c2c0e
projeto-miguel <miguel2020carneiro@gmail.com>
2024-06-11 22:51:55 -0300

    funcionamento das rotas e proteção de pastas

--- mp_teste/api/index.php
+++ mp_teste/api/index.php
@@ -1,24 +1,43 @@
 <?php
 
-
 define('_API_BASE_URI','http://localhost/bike-sports-gaspar/mp_teste/api');
 
 require_once __DIR__ . '/private/utilities/Response.php';
 
+//TREATING THE URI
 $exploded_uri = explode('/', $_GET['url']);
 
-print_r($exploded_uri);die();
+for ($i=0; $i < count($exploded_uri); $i++) { 
+    $exploded_uri[$i] = '/' . ucfirst($exploded_uri[$i]);
+}
 
+$requested_uri = '';
+
+foreach ($exploded_uri as $key => $value) {
+    $requested_uri .= $value;
+}
+
+//APLLYING ROUTE
 define('_ROUTES',[
     '/' => '',
-    '/Users' => '/public/users/',
-    '/Users/Get' => '/public/users/get'
+    '/Users' => '\public\users\index.php',
+    '/Users/' => '\public\users\index.php',
+    '/Users/Get' => '\public\users\get.php',
+    '/Users/Get/' => '\public\users\get.php'
 ]);
 
+if ($requested_uri == '/') {
+    $response = new Response(201);
+    $response->AddToResponse('data','
+        
+    ');
+}
 
+if (!isset(_ROUTES[$requested_uri])) {
+    $response = new Response($_SERVER['REQUEST_METHOD']);
+    $response->RequestError(400,"Route does not exist: " . $_SERVER['REQUEST_URI']);
+}
 
-$redirect_uri = _API_BASE_URI . _ROUTES[$_GET['url']];
+$redirect_uri = __DIR__ . _ROUTES[$requested_uri];
 
-echo __DIR__ . _ROUTES[$_GET['url']];
-echo '<br>';
-echo $redirect_uri;
+require $redirect_uri;
