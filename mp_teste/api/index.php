a033a9c2c738ef927da3ce98deb9e62a19b21388
projeto-miguel <miguel2020carneiro@gmail.com>
2024-07-04 23:07:16 -0300

    inicio do roteador

--- mp_teste/api/index.php
+++ mp_teste/api/index.php
@@ -1,68 +1,17 @@
 <?php
 namespace Api;
 
+use Api\Private\Router\Router;
 use Api\Private\Utilities\Response;
 
 define('_START_TIME',microtime(true));
 define('_CONF',parse_ini_file(__DIR__ . '/src/private/utilities/.ini', true)); 
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-
-//TREATING THE URI
-$exploded_uri = explode('/', $_GET['uri']);
-
-//EXPLODING THE URI AND CONVERTING TO AN ARRAY, APPENDING '/' AS THE FIRST CHAR FOR ALL CELLS
-//PLUS, CONVERTING TO UPPER CASE
-for ($i=0; $i < count($exploded_uri); $i++) { 
-    $exploded_uri[$i] = '/' . strtoupper($exploded_uri[$i]);
-}
-
-$requested_uri = '';
-
-//SETTING THE TREATED URI TO A VARIABLE
-foreach ($exploded_uri as $key => $value) {
-    $requested_uri .= $value;
-}
-
-$_GET['uri'] = $requested_uri;
-
-//APLLYING ROUTE
-define('_ROUTES',[
-    '/' => '',
-    '/USERS' => '\src\public\users\index.php',
-    '/USERS/' => '\src\public\users\index.php',
-    '/USERS/GET' => '\src\public\users\get.php',
-    '/USERS/GET/' => '\src\public\users\get.php',
-    '/USERS/GETBYID/' => '\src\public\users\getbyid.php',
-    '/USERS/GETBYID' => '\src\public\users\getbyid.php',
-    '/USERS/CREATE' => '\src\public\users\create.php',
-    '/USERS/CREATE/' => '\src\public\users\create.php',
-    '/USERS/DELETESOFT' => '\src\public\users\deletesoft.php',
-    '/USERS/DELETESOFT/' => '\src\public\users\deletesoft.php',
-    '/USERS/DELETEHARD/' => '\src\public\users\deletehard.php',
-    '/USERS/DELETEHARD' => '\src\public\users\deletehard.php',
-    '/USERS/UPDATE' => '\src\public\users\update.php',
-    '/USERS/UPDATE/' => '\src\public\users\update.php',
-    '/USERS/UPDATEPROP/' => '\src\public\users\updateprop.php',
-    '/USERS/UPDATEPROP' => '\src\public\users\updateprop.php',
-    '/PRODUCTS' => '\src\public\products\index.php',
-    '/PRODUCTS/' => '\src\public\products\index.php',
-    '/PRODUCTS/GET' => '\src\public\products\get.php',
-    '/PRODUCTS/GET/' => '\src\public\products\get.php',
-    '/PRODUCTS/GETBYID/' => '\src\public\products\getbyid.php',
-    '/PRODUCTS/GETBYID' => '\src\public\products\getbyid.php',
-    '/PRODUCTS/CREATE' => '\src\public\products\create.php',
-    '/PRODUCTS/CREATE/' => '\src\public\products\create.php',
-    '/PRODUCTS/DELETESOFT' => '\src\public\products\deletesoft.php',
-    '/PRODUCTS/DELETESOFT/' => '\src\public\products\deletesoft.php',
-    '/PRODUCTS/DELETEHARD/' => '\src\public\products\deletehard.php',
-    '/PRODUCTS/DELETEHARD' => '\src\public\products\deletehard.php',
-    '/PRODUCTS/UPDATE' => '\src\public\products\update.php',
-    '/PRODUCTS/UPDATE/' => '\src\public\products\update.php',
-    '/PRODUCTS/UPDATEPROP/' => '\src\public\products\updateprop.php',
-    '/PRODUCTS/UPDATEPROP' => '\src\public\products\updateprop.php',
-]);
+$u = 'asd/am.am/22/a//';
+$router = new Router;
+$uri = $router->FormatURI($u);
+echo $uri;die();
 
 if ($requested_uri == '/') {
     $response = new Response($_SERVER['REQUEST_METHOD'],$_GET);
--- mp_teste/api/src/private/router/Router.php
+++ mp_teste/api/src/private/router/Router.php
@@ -1,2 +1,60 @@
 <?php
-namespace Router;
+namespace Api\Private\Router;
+
+class Router{
+    private const _ROUTES = [
+        '/USERS' => '\src\public\users\index.php',
+        '/USERS/' => '\src\public\users\index.php',
+        '/USERS/GET' => '\src\public\users\get.php',
+        '/USERS/GET/' => '\src\public\users\get.php',
+        '/USERS/GETBYID/' => '\src\public\users\getbyid.php',
+        '/USERS/GETBYID' => '\src\public\users\getbyid.php',
+        '/USERS/CREATE' => '\src\public\users\create.php',
+        '/USERS/CREATE/' => '\src\public\users\create.php',
+        '/USERS/DELETESOFT' => '\src\public\users\deletesoft.php',
+        '/USERS/DELETESOFT/' => '\src\public\users\deletesoft.php',
+        '/USERS/DELETEHARD/' => '\src\public\users\deletehard.php',
+        '/USERS/DELETEHARD' => '\src\public\users\deletehard.php',
+        '/USERS/UPDATE' => '\src\public\users\update.php',
+        '/USERS/UPDATE/' => '\src\public\users\update.php',
+        '/USERS/UPDATEPROP/' => '\src\public\users\updateprop.php',
+        '/USERS/UPDATEPROP' => '\src\public\users\updateprop.php',
+        '/PRODUCTS' => '\src\public\products\index.php',
+        '/PRODUCTS/' => '\src\public\products\index.php',
+        '/PRODUCTS/GET' => '\src\public\products\get.php',
+        '/PRODUCTS/GET/' => '\src\public\products\get.php',
+        '/PRODUCTS/GETBYID/' => '\src\public\products\getbyid.php',
+        '/PRODUCTS/GETBYID' => '\src\public\products\getbyid.php',
+        '/PRODUCTS/CREATE' => '\src\public\products\create.php',
+        '/PRODUCTS/CREATE/' => '\src\public\products\create.php',
+        '/PRODUCTS/DELETESOFT' => '\src\public\products\deletesoft.php',
+        '/PRODUCTS/DELETESOFT/' => '\src\public\products\deletesoft.php',
+        '/PRODUCTS/DELETEHARD/' => '\src\public\products\deletehard.php',
+        '/PRODUCTS/DELETEHARD' => '\src\public\products\deletehard.php',
+        '/PRODUCTS/UPDATE' => '\src\public\products\update.php',
+        '/PRODUCTS/UPDATE/' => '\src\public\products\update.php',
+        '/PRODUCTS/UPDATEPROP/' => '\src\public\products\updateprop.php',
+        '/PRODUCTS/UPDATEPROP' => '\src\public\products\updateprop.php',
+    ];
+
+    public function FormatURI(string $uri){
+        $exploded_uri = explode('/', $uri);
+        
+        for ($i=0; $i < count($exploded_uri); $i++) { 
+            $exploded_uri[$i] = '/' . strtoupper($exploded_uri[$i]);
+        }
+
+        $requested_uri = '';
+
+        foreach ($exploded_uri as $key => $value) {
+            $requested_uri .= $value;
+        }
+
+        if(substr($requested_uri, -1) == '/'){
+            return $requested_uri;
+        }
+
+        $requested_uri .= '/';
+        return $requested_uri;
+    }
+}
