cd382129b8ebb51635684a23355e019468805be3
santozzz <davd.c9ol@gmail.com>
2024-07-24 17:58:51 -0300

    Eu David arrumei o card-produtos para ficar mais adequade

--- app/public/assets/components/card-produto.php
+++ app/public/assets/components/card-produto.php
@@ -9,21 +9,23 @@
         <?php else: ?>
             <?php foreach($produtos as $p): ?>
                 <div class="produto">
-                    <div class="img_produto">
-                        <a href="infos-produto.php?codigo=<?= $p['id'] ?>" style="width: 100%;"><img src="upload/produtos/<?= $p['img1'] ?>" style="width: 100%; border-radius: 5%;" class="produto_img"></a>
-                    </div>
-                    <div class="nome_produto">
-                        <p class="bold"><?= $p['nome'] ?></p>
-                        <p class="regular">(cod. <?= $p['id'] ?>)</p>
-                    </div>
-                    <div class="valor_produto">
-                        <p class="bold">R$ <?= $p['valor'] ?></p>
-                    </div>
-                    <div class="botao_comprar">
-                        <a href="infos-produto.php?codigo=<?= $p['id'] ?>" class="btn_comprar bold">Comprar</a>
-                    </div>
-                    <div class="ver-mais">
-                        <p>Veja mais em: <a href="">bicicletas</a></p>
+                    <div class="produto-content">
+                        <div class="img_produto">
+                            <a href="infos-produto.php?codigo=<?= $p['id'] ?>"><img src="upload/produtos/<?= $p['img1'] ?>" style="width: 100%; border-radius: 5%;" class="produto_img"></a>
+                        </div>
+                        <div class="nome_produto">
+                            <p class="bold"><?= $p['nome'] ?></p>
+                            <p class="regular">(cod. <?= $p['id'] ?>)</p>
+                        </div>
+                        <div class="valor_produto">
+                            <p class="bold">R$ <?= $p['valor'] ?></p>
+                        </div>
+                        <div class="botao_comprar">
+                            <a href="infos-produto.php?codigo=<?= $p['id'] ?>" class="btn_comprar bold">Comprar</a>
+                        </div>
+                        <div class="ver-mais">
+                            <p>Veja mais em: <a href="">bicicletas</a></p>
+                        </div>
                     </div>
                 </div>
             <?php endforeach; ?>
