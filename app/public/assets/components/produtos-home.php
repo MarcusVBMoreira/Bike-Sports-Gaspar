867fd574e23d6e07a8117c4f2c661e4e0293a85c
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-08-22 10:34:25 -0300

    atualizações na home

--- app/public/assets/components/produtos-home.php
+++ app/public/assets/components/produtos-home.php
@@ -10,13 +10,37 @@
                     <?php
                         require('assets/components/card-produto.php');
                     ?>
-                    <div class="banners">
-                        <div class="banner-1">
-
-                        </div>
-                        <div class="banner-2">
-
-                        </div>
+                    <div class="container-categories">
+                        <a href="">
+                            <div class="overlay">
+                                <p>Ferramentas</p>   
+                            </div>
+                            <img src="assets/img/Categories/ferramentas.jpg" alt="">
+                        </a>
+                        <a href="">
+                            <div class="overlay">
+                                <p>Componentes</p>   
+                            </div>
+                            <img src="assets/img/Categories/componentes.jpg" alt="">
+                        </a>
+                        <a href="">
+                            <div class="overlay">
+                                <p>Vestuário</p>   
+                            </div>
+                            <img src="assets/img/Categories/vestuario.jpg" alt="">
+                        </a>
+                        <a href="">
+                            <div class="overlay">
+                                <p>Acessórios</p>   
+                            </div>
+                            <img src="assets/img/Categories/acessorios.jpg" alt="">
+                        </a>
+                        <a href="">
+                            <div class="overlay">
+                                <p>Suplementos</p>   
+                            </div>
+                            <img src="assets/img/Categories/suplementos.webp" alt="">
+                        </a>
                     </div>
                     <?php
                         require('assets/components/card-produto.php');
@@ -24,5 +48,47 @@
                 </div>
             </div>
         </section>
+        <style>
+            .container-categories{
+                margin: 0 auto;
+                width: 100%;
+                height: 30vh;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+            }
+            .container-categories a{
+                position: relative;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 19%;
+                height: 80%;
+                background-color: #000;
+            }
+            .overlay{
+                position: absolute;
+                height: 100%;
+                width: 100%;
+                background-color: rgba(0, 0, 0, 0.411);
+                border-radius: 5px;
+                display: flex;
+                justify-content: end;
+                align-items: flex-start;
+                padding: 10px;
+            }
+            .overlay p{
+                color: #fff;
+                font-size: 20px;
+                font-weight: 500;
+                text-transform: uppercase;
+                letter-spacing: 1px;
+            }
+            .container-categories a img{
+                width: 100%;
+                height: 100%;
+                border-radius: 5px;
+            }
+        </style>
     </body>
 </html>
