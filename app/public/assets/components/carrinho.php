03ab6ac583d8084def77e702d1efcd4ab5578221
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-08-27 09:03:11 -0300

    atualização carrinho

--- app/public/assets/components/carrinho.php
+++ app/public/assets/components/carrinho.php
@@ -25,23 +25,24 @@
                     array_push($produtos, $produto['results']);
                 }
                 
+
+                
                 foreach($produtos as $p){ ?>
                     <div class="container-carrinho">
                         <div class="pedido">
                             <div class="delete" role="link">
                                 <a href="remover_carrinho.php?id=<?= $p['id'] ?>"><i class="fa-regular fa-circle-xmark"></i></a>
-                                
                             </div>
                             <div class="produto">
-                                <img src="upload/produtos/<?= $p['img1'] ?>" alt="">
+                                <div class="content-img">
+                                    <img src="upload/produtos/<?= $p['img1'] ?>" alt="">
+                                </div>
                                 <div class="desc">
                                     <div class="nome">
                                         <p class="bold"><?= $p['nome'] ?></p>
                                     </div>
                                     <div class="valor">
                                         <p>Total produto: R$ <?= $p['valor'] ?></p>
-                                        <p>Total Frete: R$ </p>
-                                        <p>Total: R$</p>
                                     </div>
                                 </div>
                             </div>
@@ -51,7 +52,41 @@
                 <?php 
                 }
             ?>
-            
+            <div class="content-compra">
+                <div class="compra">
+                    <p>Valor dos produtos: R$</p>
+                    <button>Comprar</button>
+                </div>
+                <div class="line-compra"></div>
+            </div>
+            <style>
+                .content-compra{
+                    width: 80%;
+                    height: 15vh;
+                }
+                .compra{
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    width: 100%;
+                    height: 50%;
+                }
+                .compra button{
+                    border-radius: 5px;
+                    padding: 15px;
+                    width: 20%;
+                    border: none;
+                    font-size: 15px;
+                    color: #fff;
+                    text-transform: uppercase;
+                    background-color: #ec5615;
+                    font-weight: 600;
+                    letter-spacing: 1px;
+                }
+                .line-compra{
+                    border-bottom: 1px solid #fff;
+                }
+            </style>
         <?php else: ?>
             
         <?php endif; ?>
